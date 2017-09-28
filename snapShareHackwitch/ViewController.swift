815f1bdd108a82145690abd6f06ee9d22913b649
elizabeth-anderson <student@elizabeths-computer.local>
2017-09-28 12:45:31 -0500

    set up collection view 1/2

--- snapShareHackwitch/ViewController.swift
+++ snapShareHackwitch/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    var images = [UIImage]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,20 @@ class ViewController: UICollectionViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+       return images.count
+    }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+        if let imageView = cell.viewWithTag(100) as? UIImageView
+        {
+            imageView.image = images[indexPath.item]
+            
+        }
+        return cell
+    }
 
 }
 
