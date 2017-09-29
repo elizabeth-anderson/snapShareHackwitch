c732be7d23ff76fde4fd70f387136652f40b3653
elizabeth-anderson <student@elizabeths-computer.local>
2017-09-29 12:30:57 -0500

    set up image picker

--- snapShareHackwitch/ViewController.swift
+++ snapShareHackwitch/ViewController.swift
@@ -12,15 +12,31 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
     
     var images = [UIImage]()
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = "Snap Share"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+       
+    }
+    @objc func importPicture()
+    {
+      let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any])
+    {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage
+        {
+            dismiss(animated: true, completion: nil)
+            images.insert(image, at: 0)
+            collectionView?.reloadData()
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
     {
        return images.count
