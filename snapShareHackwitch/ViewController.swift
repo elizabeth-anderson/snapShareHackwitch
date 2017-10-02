f4e411366bc048152fff82f02e1d4a68850c9ad5
elizabeth-anderson <student@elizabeths-computer.local>
2017-10-02 12:32:44 -0500

    added multipeer delegation protocols

--- snapShareHackwitch/ViewController.swift
+++ snapShareHackwitch/ViewController.swift
@@ -7,16 +7,17 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-    
-    var images = [UIImage]()
+class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, MCSessionDelegate, MCBrowserViewControllerDelegate {
+   var images = [UIImage]()
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
         title = "Snap Share"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showConnectionPrompt))
        
     }
     @objc func importPicture()
@@ -27,6 +28,25 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         present(picker, animated: true, completion: nil)
     }
     
+    @objc func showConnectionPrompt()
+    {
+        let alert = UIAlertController(title: "Connection Type", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Host a session", style: .default, handler: startHosting))
+        alert.addAction(UIAlertAction(title: "Join a session", style: .default, handler: joinSession))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func startHosting(action: UIAlertAction)
+    {
+    }
+    
+    func joinSession(action: UIAlertAction)
+    {
+    }
+    
+
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any])
     {
         if let image = info[UIImagePickerControllerEditedImage] as? UIImage
@@ -36,7 +56,50 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
             collectionView?.reloadData()
         }
     }
-
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState)
+    {
+        switch state {
+        case MCSessionState.connected:
+            print("Connected: \(peerID.displayName)")
+        case MCSessionState.connecting:
+            print("Connecting: \(peerID.displayName)")
+        case MCSessionState.notConnected:
+            print("Not Connected: \(peerID.displayName)")
+        }
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID)
+    {
+        if let image = UIImage(data: data) {
+            DispatchQueue.main.async { [unowned self] in
+                self.images.insert(image, at: 0)
+                self.collectionView?.reloadData()
+            }
+        }
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID)
+    {
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress)
+    {
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?)
+    {
+    }
+    
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController)
+    {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController)
+    {
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
     {
        return images.count
